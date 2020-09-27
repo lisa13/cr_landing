2fb09e89f95cdf20ecddd39d35948a94dc689e86
lisa13 <lisamanukyan@gmail.com>
2020-09-27 22:02:02 +0400

    https

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 // Get dependencies
 const express = require("express");
 const path = require("path");
-const http = require("http");
+const https = require("https");
 const fs = require("fs");
+const compression = require("compression");
 
 const bodyParser = require("body-parser");
 const app = express();
 
-const server = http.createServer(app);
+const credentials = {
+    key: fs.readFileSync(path.resolve(`${__dirname}/certs/server.key`)),
+    cert: fs.readFileSync(path.resolve(`${__dirname}/certs/server.pem`))
+};
+
+app.use(compression());
+
+const server = https.createServer(credentials, app);
 
 // Parsers for POST data
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 // Point static path to dist
 app.use(express.static(path.join(__dirname, "")));
